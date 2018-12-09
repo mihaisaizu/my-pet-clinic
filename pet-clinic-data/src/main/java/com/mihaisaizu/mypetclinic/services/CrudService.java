9ad508f4dd920d69a1a606a90e9035dfaef6428f
mihaisaizu <mihaisorinsaizu@gmail.com>
2018-12-09 17:34:38 +0200

    implemented Map Based Services. colses #5

--- pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/CrudService.java
@@ -10,7 +10,7 @@ public interface CrudService<T, ID> {
 
     T save(T object);
 
-    void delete(T ojbect);
+    void delete(T object);
 
-    void deleteById(T object);
+    void deleteById(ID id);
 }
---
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.mihaisaizu.mypetclinic.services.map;
+
+import com.mihaisaizu.mypetclinic.model.Vet;
+import com.mihaisaizu.mypetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
