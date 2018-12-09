764609b233587c47c204b2c77b37829fdb146486
mihaisaizu <mihaisorinsaizu@gmail.com>
2018-12-09 13:45:33 +0200

    Added BaseEntity class, closes #9

--- pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.mihaisaizu.mypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.mihaisaizu.mypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
