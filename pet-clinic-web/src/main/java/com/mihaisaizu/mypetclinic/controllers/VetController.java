30284cc6644cbd7e3f3623b49fd035726d2b1846
mihaisaizu <mihaisorinsaizu@gmail.com>
2018-12-21 22:02:38 +0200

    list vets on the vets index page. closes #15

--- pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.mihaisaizu.mypetclinic.controllers;
 
+import com.mihaisaizu.mypetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
