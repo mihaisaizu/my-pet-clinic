0353288277c5f39d1f5b0139bd379dbdcf6beef8
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-06 13:06:01 +0200

    Fixed i18n - internationalization file added. closes #23

--- pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/", "/index", "/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
