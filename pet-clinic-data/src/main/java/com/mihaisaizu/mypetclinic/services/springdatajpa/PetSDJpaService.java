f86d4917a1acbd636ac73c99795d60d7837f924d
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-13 10:35:01 +0200

    Added Spring Data JPA for Pet. closes #40

---
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.mihaisaizu.mypetclinic.services.springdatajpa;
+
+import com.mihaisaizu.mypetclinic.model.Pet;
+import com.mihaisaizu.mypetclinic.repositories.PetRepository;
+import com.mihaisaizu.mypetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Pet object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+}
