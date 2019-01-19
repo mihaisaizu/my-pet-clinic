ddf4d6e4b03c7f1464b6f1f54c0566024dca9ada
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-01-19 13:59:47 +0200

    Project updated to JUnit 5. closes #3

--- pet-clinic-web/src/test/java/com/mihaisaizu/mypetclinic/MyPetClinicTests.java
+++ pet-clinic-web/src/test/java/com/mihaisaizu/mypetclinic/MyPetClinicTests.java
@@ -1,14 +1,18 @@
 package com.mihaisaizu.mypetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MyPetClinicTests {
 
     @Test
-    public void contextLoads(){}
+    public void contextLoads(){
+
+    }
 }
