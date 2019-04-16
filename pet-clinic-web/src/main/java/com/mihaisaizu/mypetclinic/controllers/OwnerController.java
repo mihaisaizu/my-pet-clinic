d7cef95a7d95dd4a8a3fe5dfb07d5a24a079afd8
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-04-16 21:04:08 +0300

    display owner with BUG fixed. closes #59

--- pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/mihaisaizu/mypetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.mihaisaizu.mypetclinic.controllers;
 import com.mihaisaizu.mypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping({"/owners", "/owners.html"})
 @Controller
@@ -23,7 +26,13 @@ public class OwnerController {
 
     @RequestMapping("/find")
     public String findOwners(){
-
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerID) {
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerID));
+        return modelAndView;
     }
 }
