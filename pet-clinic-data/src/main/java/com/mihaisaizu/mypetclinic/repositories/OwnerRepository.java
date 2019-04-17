15c196364c8ec5f7022d2420bce8225c3e26f74b
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-04-17 21:52:19 +0300

    Find owner implemented. closes #52

--- pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,12 @@ package com.mihaisaizu.mypetclinic.repositories;
 import com.mihaisaizu.mypetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
+
 }
--- pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/mihaisaizu/mypetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.mihaisaizu.mypetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -20,6 +21,12 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     public OwnerMapService(PetTypeService petTypeService, PetService petService) {
         this.petTypeService = petTypeService;
         this.petService = petService;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //todo implementation
+        return null;
     }
 
     @Override
