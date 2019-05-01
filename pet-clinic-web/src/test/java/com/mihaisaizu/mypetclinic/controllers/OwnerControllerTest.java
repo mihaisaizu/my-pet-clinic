c7bf35654e8e2e32beb304282a0c17f178169d66
mihaisaizu <mihaisorinsaizu@gmail.com>
2019-05-01 12:21:32 +0300

    test for find owner/s by last name. closes #63

--- pet-clinic-web/src/test/java/com/mihaisaizu/mypetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/mihaisaizu/mypetclinic/controllers/OwnerControllerTest.java
@@ -68,11 +68,23 @@ class OwnerControllerTest {
     }
 
     @Test
-    void processFindFormForOne() throws Exception{
+    void processFindFormReturnOne() throws Exception{
         when(ownerService.findAllByLastNameLike(anyString())).thenReturn(Arrays.asList(Owner.builder().id(1L).build()));
         mockMvc.perform(get("/owners"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/owners/1"));
+    }
+
+    @Test
+    void processFindFormReturnMany() throws Exception{
+        when(ownerService.findAllByLastNameLike(anyString())).
+                thenReturn(Arrays.asList
+                        (Owner.builder().id(1L).build(),Owner.builder().id(2L).build()));
+        mockMvc.perform(get("/owners")
+                            .param("lastName", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("owners", hasSize(2)));
     }
 
     @Test
